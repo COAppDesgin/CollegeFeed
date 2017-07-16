b92c664c8e687f7b6fb0abe8c56ff4bde9846747
Tyler J. Cagle <tyler.cagle@colorado.edu>
2017-07-15 19:49:00 -0600

    CollegeFeed first app push for GitHub

---
+++ CollegeFeed/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  CollegeFeed
+//
+//  Created by Tyler Jordan Cagle on 7/15/17.
+//  Copyright © 2017 COAppDesign. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Push this item to GitHub")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
