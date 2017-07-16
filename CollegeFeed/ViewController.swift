fdf2c311fe38c04263c6a7e9ef483739d0ef58bb
Tyler J. Cagle <tyler.cagle@colorado.edu>
2017-07-15 20:01:23 -0600

    Test Change

--- CollegeFeed/ViewController.swift
+++ CollegeFeed/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Push this item to GitHub")
     }
 
     override func didReceiveMemoryWarning() {
